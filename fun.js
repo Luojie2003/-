315966d52b1a899ae1c6da876e66265560c17a2c
luojie <rowe_jason@outlook.com>
2023-04-13 11:25:18 +0800

    update function: Add

--- fun.js
+++ fun.js
@@ -7,39 +7,43 @@ let sex = document.querySelector('#select_sex');
 
 var data = [];
 
-function addDomEvs(){
-    
-}
 
 function displayAdd(nm_,age_,id_,sex_){
     let ad = document.createElement('div');
     ad.innerText = "ID: " + id_ + "姓名: " + nm_ + "年龄: " + age_ + "性别: " + sex_;
-    // console.log(display);
     display.appendChild(ad);
 }
 
 function submit(){
-    let btn = document.querySelector('#add');
-    btn.addEventListener('click', function (){
-        let nmv = nm.value;
-        let idv = id.value;
-        let agev = age.value;
-        let sexv = sex.value;
-        // console.log(sexv);
-        // console.log(nmv);
-        data[data.length]={
-            nmv,idv,agev,sexv
-        };
-        console.log(data);
-        displayAdd(nmv,agev,idv,sexv);
-    });
+    let nmv = nm.value;
+    let idv = id.value;
+    let agev = age.value;
+    let sexv = sex.value;
+    // console.log(sexv);
+    // console.log(nmv);
+    data[data.length]={
+        nmv,idv,agev,sexv
+    };
+    console.log(data);
+    displayAdd(nmv,agev,idv,sexv);
 }
-submit();
+
+function submit_clicked(){
+    let btn = document.querySelector('#add');
+    btn.addEventListener('click',submit);
+}
+submit_clicked();
 
 function resetPage() {
+    // console.log("ok");
     id.value = "";
     nm.value = "";
     age.value = "";
     sex.value = "other";
-    // display.innerHTML = "";
 }
+
+function reset_clicked(){
+    let btn = document.querySelector('#clear');
+    btn.addEventListener('click',resetPage);
+}
+reset_clicked();
